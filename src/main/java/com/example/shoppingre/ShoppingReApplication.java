aa5757aac7e8d9fa0b37e42a9dfe641f6029c6d1
postwo <codo7717@naver.com>
2024-06-18 21:36:32 +0900

    기본 세팅 완료

---
+++ src/main/java/com/example/shoppingre/ShoppingReApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingReApplication.class, args);
+    }
+
+}
